51aaa710ab53d1e44cada00a715a2c2923b9ef32
lizhealy <lizhealy8@gmail.com>
2017-01-24 12:45:24 -0500

    Initial Commit

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 32.781825, -79.939026
+        
+        let latitude: CLLocationDegrees = 32.781825
+        let longitude: CLLocationDegrees = -79.939026
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "My House"
+        annotation.subtitle = "16 Montagu St"
+        annotation.coordinate = location
+        
+        mapView.addAnnotation(annotation)
+        
+        var lpg = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        lpg.minimumPressDuration = 2
+        
+        mapView.addGestureRecognizer(lpg)
+        
+        
+    }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: self.mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "New Place"
+        annotation.subtitle = "Here"
+        
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
